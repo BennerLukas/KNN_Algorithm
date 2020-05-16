a188bfb586cb747a9e1c567477351eb6863a9cf4
Lukas <lukas_benner@web.de>
2020-05-16 13:57:38 +0200

    add testing and structure

---
+++ src/KNN_scratch.py
@@ -0,0 +1,11 @@
+import  pandas as pd
+import numpy as np 
+import math
+
+# calculate_euclidean_distance
+def calc_distance(x1, x2):
+    distance = math.sqrt((x1 - x2 )**2)
+    return distance
+
+def get_neighbour(liste):
+    pass
